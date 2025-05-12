4babddd21e7b857e18af48487d4cae3abeff15b9
Samarth Sharma <110784602+bugsum@users.noreply.github.com>
2025-05-12 18:48:37 +0530

    Fixed next config (skill issue ig)

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 import type { NextConfig } from 'next';
-import createMDX from '@next/mdx';
 
 const nextConfig: NextConfig = {
-    pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
     trailingSlash: true,
     output: 'export',
     images: {
@@ -10,9 +8,5 @@ const nextConfig: NextConfig = {
     },
 };
 
-const withMDX = createMDX({
-    extension: /\.(md|mdx)$/,
-});
-
 // Merge MDX config with Next.js config
-export default withMDX(nextConfig);
+export default nextConfig;
